b9343a4213cac9fa5d8938c432e8bb789369d269
Arianne Malagayo <a.malagayo@tamu.edu>
2023-08-31 01:28:24 -0500

    added help msg

--- static/js/demo-3.js
+++ static/js/demo-3.js
@@ -14,11 +14,11 @@ document.querySelector('#send-msg').addEventListener('click', (e) => {
   check_words = words.some(word => message_text.includes(word));
   console.log(check_words)
 
-  if (check_words == false) {
-      var newBubble = document.createElement('div');
-      newBubble.innerHTML = '<div class="row justify-content-end"> <div class="message message-out">'+message_text+'</div> </div>'
-      lastMessage.parentNode.appendChild(newBubble, lastMessage.nextSibling);
+  var newBubble = document.createElement('div');
+  newBubble.innerHTML = '<div class="row justify-content-end"> <div class="message message-out">'+message_text+'</div> </div>'
+  lastMessage.parentNode.appendChild(newBubble, lastMessage.nextSibling);
 
+  if (check_words == false) {
       var responseBubble = document.createElement('div');
       responseBubble.innerHTML = '<div class="row justify-content-start"> <div class="message message-in">'+'what\'s on your mind?'+'</div> </div>'
       lastMessage.parentNode.appendChild(newBubble, lastMessage.nextSibling);
@@ -26,8 +26,13 @@ document.querySelector('#send-msg').addEventListener('click', (e) => {
          lastMessage.parentNode.appendChild(responseBubble, lastMessage.nextSibling);},2000);
   }
 
-  if(check_words == true) {
-
+  if (check_words == true) {
+      alert_message = "Help is available. Speak with someone today. We have dialed Suicide Prevention Hotline (998) to connect you to someone."
+      var notifyBubble = document.createElement('div');
+      notifyBubble.innerHTML = '<div class="row justify-content-start"> <div class="message message-alert">'+alert_message+'</div> </div>'
+      lastMessage.parentNode.appendChild(notifyBubble, lastMessage.nextSibling);
+      setTimeout(()=> {
+         lastMessage.parentNode.appendChild(notifyBubble, lastMessage.nextSibling);},5000);
   }
 
 });
